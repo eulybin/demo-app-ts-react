78fe1304d8fe85d51a0093c7b950c5adc16462bc
Egor Ulybin <egorulybin@gmail.com>
2025-10-19 12:46:51 +0200

    button/anchor component with ts props

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,16 @@
+// import Input from './components/Input';
+import Button from './components/Button';
+
 function App() {
-  return <h1>Let's get started!</h1>;
+    return (
+        <main>
+            {/* <Input id='name' label='Your name' type='text' />
+            <Input id='age' label='Your age' type='number' /> */}
+
+            <Button elem='button' />
+            <Button elem='anchor' />
+        </main>
+    );
 }
 
 export default App;
---
+++ src/components/Button.tsx
@@ -0,0 +1,25 @@
+import type { ComponentPropsWithoutRef } from 'react';
+
+type ButtonProps = {
+    elem: 'button';
+} & ComponentPropsWithoutRef<'button'>;
+
+type AnchorProps = {
+    elem: 'anchor';
+} & ComponentPropsWithoutRef<'a'>;
+
+export default function Button(props: ButtonProps | AnchorProps) {
+    if (props.elem === 'anchor') {
+        return (
+            <a className='button' href='https://google.com' target='_blank' {...props}>
+                Anchor
+            </a>
+        );
+    }
+
+    return (
+        <button className='button' {...props}>
+            Test
+        </button>
+    );
+}
